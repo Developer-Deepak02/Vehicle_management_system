3c34da356ca64b2347cf9897fd1fe14374158b72
Deepak Sharma <db23806@gmail.com>
2026-08-10 16:55:17 +0530

    feat: add vehicle retrieval, update, and deletion functionalities

--- server/controllers/vehicleController.js
+++ server/controllers/vehicleController.js
@@ -61,3 +61,82 @@ export const getAllVehicles = async (req, res) => {
 		res.status(500).json({ message: "Server error" });
 	}
 };
+
+// get vehicle
+
+export const getVehicle = async (req, res) => {
+	try {
+		const vehicle = await Vehicle.findById(req.params.id);
+		if (!vehicle) {
+			return res.status(404).json({ message: "Vehicle not found" });
+		}
+		res.status(200).json(vehicle);
+	} catch (error) {
+		res.status(500).json({ message: "Server error" });
+	}
+};
+
+// update vehicle
+export const updateVehicle = async (req, res) => {
+	const vehicleId = req.params.id;
+	const {
+		vehicleName,
+		vehicleModel,
+		vehicleYear,
+		vehicleType,
+		vehicleDescription,
+		vehiclePhoto,
+	} = req.body;
+
+	try {
+		const vehicle = await Vehicle.findById(vehicleId);
+		if (!vehicle) {
+			return res.status(404).json({ message: "Vehicle not found" });
+		}
+		if (vehicleName !== undefined) {
+			vehicle.vehicleName = vehicleName;
+		}
+
+		if (vehicleModel !== undefined) {
+			vehicle.vehicleModel = vehicleModel;
+		}
+		if (vehicleYear !== undefined) {
+			vehicle.vehicleYear = vehicleYear;
+		}
+		if (vehicleType !== undefined) {
+			vehicle.vehicleType = vehicleType;
+		}
+		if (vehicleDescription !== undefined) {
+			vehicle.vehicleDescription = vehicleDescription;
+		}
+		if (vehiclePhoto !== undefined) {
+			vehicle.vehiclePhoto = vehiclePhoto;
+		}
+		vehicle.updatedBy = req.user._id;
+		vehicle.updatedOn = Date.now();
+		await vehicle.save();
+		res.status(200).json(vehicle);
+	} catch (error) {
+		res.status(500).json({ message: "Server error" });
+	}
+};
+
+// delete vehicle
+
+export const deleteVehicle = async (req, res) => {
+	try {
+		const vehicle = await Vehicle.findById(req.params.id);
+		if (!vehicle) {
+			return res.status(404).json({ message: "Vehicle not found" });
+		}
+		if (vehicle.status === "assigned") {
+			return res
+				.status(400)
+				.json({ message: "Vehicle is already assigned try again later" });
+		}
+		await vehicle.deleteOne();
+		res.status(200).json({ message: "Vehicle deleted successfully" });
+	} catch (error) {
+		res.status(500).json({ message: "Server error" });
+	}
+};
--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -11,9 +11,15 @@ const router = express.Router();
 import {
 	createVehicle,
 	getAllVehicles,
+	getVehicle,
+	updateVehicle,
+	deleteVehicle,
 } from "../controllers/vehicleController.js";
 
 router.post("/create-vehicle", protect, admin, createVehicle);
 router.get("/all-vehicles", protect, adminAndManager, getAllVehicles);
+router.get("/:id", protect, adminAndManager, getVehicle);
+router.put("/update-vehicle/:id", protect, admin, updateVehicle);
+router.delete("/delete-vehicle/:id", protect, admin, deleteVehicle);
 
 export default router;
